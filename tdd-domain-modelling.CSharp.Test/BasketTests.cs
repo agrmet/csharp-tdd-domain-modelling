b310a49aa1821989b37fb3d4af46e72132e60cba
agrmet <agron-metaj@hotmail.com>
2024-08-14 13:54:19 +0200

    More passing tests for add

--- tdd-domain-modelling.CSharp.Test/BasketTests.cs
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -17,5 +17,39 @@ namespace tdd_domain_modelling.CSharp.Test
             // assert
             Assert.True(result);
         }
+        [Test]
+        public void AddSeveralTest()
+        {
+            // arrange
+            Basket basket = new();
+            string product1 = "Pear";
+            int price1 = 10;
+            string product2 = "Coffee";
+            int price2 = 55;
+            // act
+            var result1 = basket.Add(product1, price1);
+            var result2 = basket.Add(product2, price2);
+
+            // assert
+            Assert.True(result1);
+            Assert.True(result2);
+
+        }
+
+        [Test]
+        public void AddConflictingTest()
+        {
+            // arrange
+            Basket basket = new();
+            string product = "Pear";
+            int price = 10;
+            // act
+            var result1 = basket.Add(product, price);
+            var result2 = basket.Add(product, price);
+
+            // assert
+            Assert.True(result1);
+            Assert.False(result2);
+        }
     }
 }
