19b5f70c6c155cf968dd144fc70b8502ec9b392e
agrmet <agron-metaj@hotmail.com>
2024-08-14 13:51:23 +0200

    Testing add function passing

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,16 @@
+﻿
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        private Dictionary<string, int> _basket = new ();
+        public Dictionary<string, int> basket { get { return _basket; } }
+
+        public bool Add(string product, int price)
+        {
+            if (basket.ContainsKey(product)) return false;
+            _basket.Add(product, price);
+            return true;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    public class BasketTests
+    {
+        [Test]
+        public void AddTest()
+        {
+            // arrange
+            Basket basket = new();
+            string product = "Pear";
+            int price = 10;
+            // act
+            var result = basket.Add(product, price);
+            // assert
+            Assert.True(result);
+        }
+    }
+}
